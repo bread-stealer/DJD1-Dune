9869f10b0d387f2ae78229b68e7625a6b0ffb72a
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-06-01 00:50:44 +0100

    Add OnFootstep animation event to AnimEventBridge

--- Assets/Scripts/Player/AnimEventBridge.cs
+++ Assets/Scripts/Player/AnimEventBridge.cs
@@ -4,10 +4,17 @@ public class AnimEventBridge : MonoBehaviour
 {
     // PlayerAttack subscribes to this event in its Awake
     public event System.Action OnAttackHitEvent;
+    public event System.Action OnFootstepEvent;
 
     // Called by Animation Event on the child sprite
     public void OnAttackHit()
     {
         OnAttackHitEvent?.Invoke();
+    }
+
+    // Called by Animation Event on the walk animation
+    public void OnFootstep()
+    {
+        OnFootstepEvent?.Invoke();
     }
 }
