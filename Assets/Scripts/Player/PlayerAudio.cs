cccd61198bab724c05e496a4b0f6c04e0e472bf6
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-06-01 00:27:11 +0100

    Replace AudioClip with SFXClip for individual volume control per SFX

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+// Hooks into existing events to trigger audio
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerShield))]
+public class PlayerAudio : MonoBehaviour
+{
+    [Header("SFX Clips")]
+    [SerializeField] private SFXClip attackClip;
+    [SerializeField] private SFXClip hitClip;
+    [SerializeField] private SFXClip deathClip;
+    [SerializeField] private SFXClip jumpClip;
+    [SerializeField] private SFXClip landClip;
+    [SerializeField] private SFXClip shieldOnClip;
+    [SerializeField] private SFXClip shieldOffClip;
+    [SerializeField] private SFXClip shieldBlockClip;
+    [SerializeField] private SFXClip shieldBrokenClip;
+
+    private PlayerHealth _health;
+    private PlayerShield _shield;
+
+    private void Awake()
+    {
+        _health = GetComponent<PlayerHealth>();
+        _shield = GetComponent<PlayerShield>();
+    }
+
+    private void OnEnable()
+    {
+        _health.OnHealthChanged += HandleHealthChanged;
+        _health.OnDeath += HandleDeath;
+        _shield.OnShieldActivated += HandleShieldOn;
+        _shield.OnShieldDeactivated += HandleShieldOff;
+        _shield.OnShieldBlocked += HandleShieldBlock;
+        _shield.OnShieldBroken += HandleShieldBroken;
+    }
+
+    private void OnDisable()
+    {
+        _health.OnHealthChanged -= HandleHealthChanged;
+        _health.OnDeath -= HandleDeath;
+        _shield.OnShieldActivated -= HandleShieldOn;
+        _shield.OnShieldDeactivated -= HandleShieldOff;
+        _shield.OnShieldBlocked -= HandleShieldBlock;
+        _shield.OnShieldBroken -= HandleShieldBroken;
+    }
+
+    // Called by Animation Event on the attack animation
+    public void PlayAttackSFX()
+    {
+        AudioManager.Instance?.PlaySFX(attackClip.clip, attackClip.volume);
+    }
+
+    // Called by PlayerController when landing
+    public void PlayLandSFX()
+    {
+        AudioManager.Instance?.PlaySFX(landClip.clip, landClip.volume, randomPitch: false);
+    }
+
+    // Called by PlayerController when jumping
+    public void PlayJumpSFX()
+    {
+        AudioManager.Instance?.PlaySFX(jumpClip.clip, jumpClip.volume);
+    }
+
+    private void HandleHealthChanged(float current, float max)
+    {
+        AudioManager.Instance?.PlaySFX(hitClip.clip, hitClip.volume);
+    }
+
+    private void HandleDeath()
+    {
+        AudioManager.Instance?.PlaySFX(deathClip.clip, deathClip.volume, randomPitch: false);
+    }
+
+    private void HandleShieldOn()
+    {
+        AudioManager.Instance?.PlaySFX(shieldOnClip.clip, shieldOnClip.volume, randomPitch: false);
+    }
+
+    private void HandleShieldOff()
+    {
+        AudioManager.Instance?.PlaySFX(shieldOffClip.clip, shieldOffClip.volume, randomPitch: false);
+    }
+
+    private void HandleShieldBlock()
+    {
+        AudioManager.Instance?.PlaySFX(shieldBlockClip.clip, shieldBlockClip.volume);
+    }
+
+    private void HandleShieldBroken()
+    {
+        AudioManager.Instance?.PlaySFX(shieldBrokenClip.clip, shieldBrokenClip.volume, randomPitch: false);
+    }
+}
